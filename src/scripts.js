ac3ef906c80d03badf0b5a0f04893e97c95bab7e
Anna Peterson <reveragb@gmail.com>
2022-11-11 16:45:51 -0700

    Add image and modify code accordingly

--- src/scripts.js
+++ src/scripts.js
@@ -2,13 +2,17 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/styles.css';
+import './css/styles.css'
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
+import './images/usflag.png'
+import './images/user.png'
+import './images/hand-pointer.png'
+import './images/questions-button.png'
 
 
-console.log('This is the JavaScript entry file - your code begins here.');
+console.log('This is the JavaScript entry file - your code begins here.')
 
 // `<li class="cards">
 //   <div class="card-container">

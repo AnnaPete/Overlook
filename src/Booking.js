350ee08c7c1a80bf24ebe9bcf3801d63c57d16dc
Anna Peterson <reveragb@gmail.com>
2022-11-12 11:39:53 -0700

    Finish booking class and tests

--- src/Booking.js
+++ src/Booking.js
@@ -1,20 +1,15 @@
+import Room from './Room'
 
-// id: "string of numbers and letters"
-// userID: number
-// date: "year/month/day"
-// roomNumber: number
+class Booking {
+  constructor(data) {
+    this.id = data.id
+    this.userID = data.userID
+    this.date = data.date
+    this.roomNumber = data.roomNumber
+  }
 
+  
 
-
-// const date = new Date()
-// let currentDay = date.getDate()
-// let currentMonth = date.getMonth() + 1
-// let currentYear = date.getFullYear()
-// let currentDate = `${currentYear}${currentMonth}${currentDay}`
-// currentDate = Number(currentDate)
-// 
-// let chargeDate = this.date.split("/")
-//   chargeDate = Number(chargeDate.join(""))
-
+}
 
 export default Booking
--- test/booking-test.js
+++ test/booking-test.js
@@ -1,3 +1,67 @@
 import chai from 'chai';
 const expect = chai.expect;
 
+let booking1 = {
+id: "5fwrgu4i7k55hl6sz",
+userID: 9,
+date: "2022/04/22",
+roomNumber: 15
+}
+
+let booking2 = {
+id: "5fwrgu4i7k55hl6t5",
+userID: 43,
+date: "2022/01/24",
+roomNumber: 24
+}
+
+let booking3 = {
+id: "5fwrgu4i7k55hl6t6",
+userID: 13,
+date: "2022/01/10",
+roomNumber: 12
+}
+
+let booking4 = {
+id: "5fwrgu4i7k55hl6t7",
+userID: 20,
+date: "2022/02/16",
+roomNumber: 7
+}
+
+let booking5 = {
+id: "5fwrgu4i7k55hl6t8",
+userID: 1,
+date: "2022/02/05",
+roomNumber: 12
+}
+
+let booking6 = {
+id: "5fwrgu4i7k55hl6t9",
+userID: 38,
+date: "2023/12/14",
+roomNumber: 14
+}
+
+describe('Booking class', function() {
+  it('should have an id', function() {
+    expect(booking1.id).to.equal("5fwrgu4i7k55hl6sz");
+    expect(booking2.id).to.equal("5fwrgu4i7k55hl6t5");
+  });
+
+  it('should have a user id', function() {
+    expect(booking3.userID).to.equal(13);
+    expect(booking4.userID).to.equal(20);
+  });
+
+  it('should have a date', function() {
+    expect(booking4.date).to.equal("2022/02/16");
+    expect(booking5.date).to.equal("2022/02/05");
+    expect(booking6.date).to.equal("2023/12/14");
+  });
+
+  it('should have a room number', function() {
+    expect(booking1.roomNumber).to.equal(15);
+    expect(booking3.roomNumber).to.equal(12);
+  });
+})

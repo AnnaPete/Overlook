34e6e456d793c5009f2a78dd07b24def7da78891
Anna Peterson <reveragb@gmail.com>
2022-11-12 16:12:03 -0700

    Create mock data

---
+++ test/fixtures/bookings-mock.js
@@ -0,0 +1,38 @@
+export const bookings = [
+  {
+    id: "5fwrgu4i7k55hl6sz",
+    userID: 5,
+    date: "2022/04/22",
+    roomNumber: 15
+  },
+  {
+    id: "5fwrgu4i7k55hl6t5",
+    userID: 43,
+    date: "2022/01/24",
+    roomNumber: 24
+  },
+  {
+    id: "5fwrgu4i7k55hl6t6",
+    userID: 5,
+    date: "2022/01/10",
+    roomNumber: 12
+  },
+  {
+    id: "5fwrgu4i7k55hl6t7",
+    userID: 20,
+    date: "2022/02/16",
+    roomNumber: 7
+  },
+  {
+    id: "5fwrgu4i7k55hl6t8",
+    userID: 6,
+    date: "2022/02/05",
+    roomNumber: 12
+  },
+  {
+    id: "5fwrgu4i7k55hl6t9",
+    userID: 6,
+    date: "2023/12/14",
+    roomNumber: 14
+  }
+]

37735815492fb014a47dce08bdc5ed003622b32f
Anna Peterson <reveragb@gmail.com>
2022-11-12 13:08:35 -0700

    Add function to get API data

--- src/scripts.js
+++ src/scripts.js
@@ -18,13 +18,43 @@ import './images/hand-pointer.png'
 import './images/questions-button.png'
 
 // Global Variables
-
+let customers
+let rooms
+let bookings
+let currentCustomer
+const currentCustomerID = 10
 
 // Query Selectors
 
 
 // Event Listeners
-window.addEventListener('load', fetchData([customersURL, bookingsURL, roomsURL]))
+window.addEventListener('load', fetchData)
+
+
+// API Function
+function fetchData() {
+  const customersData = getData(customersURL)
+  const roomsData = getData(roomsURL)
+  const bookingsData = getData(bookingsURL)
+  Promise.all([customersData, roomsData, bookingsData])
+    .then(data => {
+      customers = data[0].customers.map((customer) => {
+        return new Customer(customer)
+      })
+      rooms = data[1].rooms.map((room) => {
+        return new Room(room)
+      })
+      bookings = data[2].bookings.map((booking) => {
+        return new Booking(booking)
+      })
+      currentCustomer = customers.find(customer => {
+        return customer.id === currentCustomerID
+      })
+      currentCustomer.filterBookings(bookings)
+    })
+    .then(() => console.log(currentCustomer))
+    .catch(err => console.log("Error: ", err))
+}
 
 // Event Handlers
 

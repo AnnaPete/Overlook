79b0403a33c61970e9eb652ec02cd2313b0ac300
Anna Peterson <reveragb@gmail.com>
2022-11-12 10:05:58 -0700

    Finish room class and tests

---
+++ src/Room.js
@@ -0,0 +1,12 @@
+const Room {
+  constructor(data) {
+    this.number = data.roomNum
+    this.roomType = data.roomType
+    this.bidet = data.hasBidet
+    this.bedSize = data.bedSize
+    this.numBeds = data.numBeds
+    this.costPerNight = data.cost
+  }
+}
+
+export default Room
